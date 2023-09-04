1ae8232caae4cf62e04c8530a4dfb0f156db91dc
MunavarPM <munavarmunna101@gmail.com>
2023-09-04 20:35:35 +0530

    Initial Commit

---
+++ WeatherApp_SwiftUi/WeatherApp_SwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_SwiftUiApp.swift
+//  WeatherApp_SwiftUi
+//
+//  Created by MUNAVAR PM on 04/09/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_SwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
